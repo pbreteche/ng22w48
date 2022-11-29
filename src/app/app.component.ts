ebdc87ef90f1be66b5e5ccb6d74fa2e59d4fb156
Pierre Bretéché <pbreteche@dawan.fr>
2022-11-29 10:48:28 +0100

    première manipulation de composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng22w48';
+  contact = {
+    firstName: 'Pierre',
+    company: 'Dawan'
+  }
+
+  get pseudoProperty() {
+    return 'retour de la fonction';
+  }
 }
