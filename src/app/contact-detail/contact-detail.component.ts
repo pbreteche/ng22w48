9f20e6d9d79837d685b7ceecb11b0ef17c13828e
Pierre Bretéché <pbreteche@dawan.fr>
2022-12-02 11:40:48 +0100

    ngClass et class binding

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -18,4 +18,19 @@ export class ContactDetailComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({contact}) => this.current = contact)
   }
+
+  // Les classes peuvent être retournée au format
+  // Liste de chaines séparées par des espaces
+  longName(): string {
+    return (this.current?.firstName?.length || 0) > 10 ? 'long' : 'short';
+  }
+
+  // Un objet, chaque propriété représente une classe
+  // la valeur booléenne détermine si la classe est présente ou non
+  longName2() {
+    return {
+      long: (this.current?.firstName?.length || 0) > 10,
+      short: (this.current?.firstName?.length || 0) <= 10,
+    }
+  }
 }
